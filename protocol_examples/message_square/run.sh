d8cd47f5ae6afa009c5bf1428bacb899b3b91055
Francisco Fleury <franmeifleury@gmail.com>
2024-11-02 17:28:13 -0300

    fixes protocol_examples

--- protocol_examples/message_square/run.sh
+++ protocol_examples/message_square/run.sh
@@ -1,3 +1,3 @@
-bash ./scripts/register_protocol.sh MessageProtocol examples.message_square.message_protocol
+bash ./scripts/register_protocol.sh MessageProtocol protocol_examples.message_square.message_protocol
 
 python3 protocol_testing.py --n 4 --protocol MessageProtocol MessageProtocol MessageProtocol MessageProtocol --pos [100,100,50] [100,-100,50] [-100,-100,50] [-100,100,50]
