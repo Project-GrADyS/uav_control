d27a362646f0ca04b38e60047963d032eb1ed33e
Francisco Fleury <franmeifleury@gmail.com>
2025-01-16 00:35:38 -0300

    Updating examples to follow 'simulation/protocol_testing.py' structure

--- protocol_examples/telemetry_square/run.sh
+++ protocol_examples/telemetry_square/run.sh
@@ -1,3 +1,3 @@
 bash ./scripts/register_protocol.sh TelemetryProtocol protocol_examples.telemetry_square.telemetry_protocol
 
-python3 protocol_testing.py --n 4 --protocol TelemetryProtocol TelemetryProtocol TelemetryProtocol TelemetryProtocol --pos [100,100,50] [100,-100,50] [-100,-100,50] [-100,100,50]
+python3 simulation/protocol_testing.py --n 4 --protocol TelemetryProtocol TelemetryProtocol TelemetryProtocol TelemetryProtocol --pos [100,100,50] [100,-100,50] [-100,-100,50] [-100,100,50]
